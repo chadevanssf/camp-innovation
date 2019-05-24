9e52edef4038dd708fadbb704e7d62d05de960fa
Chad Evans <chad.evans@salesforce.com>
2019-05-24 11:00:32 -0400

    Updating to host multiple use cases

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,12 @@ var workshops = require('../lib/workshop-definitions');
 
 var router = express.Router();
 
+// use the environment var from Heroku if set
+const DEFAULT_HOME = process.env.DEFAULT_HOME || "main";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { workshops: workshops.getDefs(workshops.main)});
+  res.render('index', { workshops: workshops.getDefs(workshops[DEFAULT_HOME])});
 });
 
 router.get('/:mix', function (req, res, next) {
