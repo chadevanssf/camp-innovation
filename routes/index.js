2b72f8bc83552176694b907826ce58daf8e92504
Chad Evans <chad.evans@salesforce.com>
2019-05-24 11:27:23 -0400

    Updating for HC

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,17 @@ var router = express.Router();
 
 // use the environment var from Heroku if set
 const DEFAULT_HOME = process.env.DEFAULT_HOME || "main";
+const DEFAULT_HOME_LAYOUT = process.env.DEFAULT_HOME_LAYOUT || "index";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { workshops: workshops.getDefs(workshops[DEFAULT_HOME])});
+  res.render(DEFAULT_HOME_LAYOUT, { workshops: workshops.getDefs(workshops[DEFAULT_HOME])});
 });
 
 router.get('/:mix', function (req, res, next) {
   if (workshops[req.params.mix]){
     // default page
-    let page = 'demos';
+    let page = DEFAULT_HOME_LAYOUT;
 
     // special cases for adoption pages, which have different treatment
     if (req.params.mix === 'adoption' || req.params.mix === 'df18adoption' || req.params.mix === 'tdx19adoption'){
